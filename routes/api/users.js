eefba0c70b9d41260e5c7582cf461116d0e3ec82
Christian Sheen <christiansheen@MacBook-Pro.local>
2020-11-10 14:17:46 -0500

    Required gravatar && bcrypt then turned our network request into an asynchronous function because some operations within the "try, catch block" returned promises (await User.findOne => to find a user by email, await bycrypt.gensalt => for encryption, await bycrypt.hash => takes password of previously defined user and encrypts it, finally we save the user to the database with => await user.save())

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const router = express.Router();
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
 const { check, validationResult } = require("express-validator/check");
+
+const User = require("../../models/User");
 
 //@route     POST api/users
 //@desc      Register user
@@ -15,12 +19,50 @@ router.post(
       "Please enter a password with 6 or more characters",
     ).isLength({ min: 6 }),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    res.send("User route");
+
+    const { name, email, password } = req.body;
+
+    try {
+      // See if the user exists
+      let user = await User.findOne({ email });
+
+      if (user) {
+        res.status(400).json({ errors: [{ msg: "User already exists" }] });
+      }
+
+      // Get users gravatar
+      const avatar = gravatar.url(email, {
+        s: "200",
+        r: "pg",
+        d: "mm",
+      });
+
+      user = new User({
+        name,
+        email,
+        avatar,
+        password,
+      });
+
+      // Encrypt password using bcrypt
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      // Return jsonWebToken // Missing
+
+      res.send("User Registered");
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server error");
+    }
   },
 );
 
