7a97e7919bb9c36e2859b2e71b5f22b567c0a9ee
Christian Sheen <christiansheen@MacBook-Pro.local>
2020-11-19 13:57:33 -0500

    Create DELETE method for main route. Here I simply added the "auth" param and proceeded to run a try catch block which returned a server error message if failed. If successfull it found and removed profile && user from database by referencing the request body ({user: req.user.id})

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -140,4 +140,22 @@ router.get("/user/:user_id", async (req, res) => {
  }
 });
 
+//@route     DELETE api/profile/
+//@desc      Delete profile, user &  posts
+//@access    Private
+
+router.delete("/", auth, async (req, res) => {
+ try {
+  // @TODO - remove users posts
+  // Remove Profile
+  await Profile.findOneAndRemove({ user: req.user.id });
+  //Remove User
+  await User.findOneAndRemove({ _id: req.user.id });
+  res.json({ msg: "User deleted" });
+ } catch (err) {
+  console.error(err.message);
+  res.status(500).send("Server Error");
+ }
+});
+
 module.exports = router;
