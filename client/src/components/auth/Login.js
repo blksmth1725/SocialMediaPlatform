1c623030fb05b5a99df2f9fe6adcceb324bab199
Christian Sheen <christiansheen@macbook-pro.chris.lan>
2021-04-09 14:59:58 -0400

    Mid creation of redux store && other boiler plate setup

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,7 +1,68 @@
-import React from "react";
+import React, { Fragment, useState } from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-  return <div>Login</div>;
+  const [formData, setFromData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    password2: "",
+  });
+
+  const { email, password } = formData;
+
+  const onChange = (e) =>
+    setFromData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = async (e) => {
+    //Prevent default behavior => which automatically calls function
+    e.preventDefault();
+    //Check if password inputs match // available to you via decunstructed fromData
+    console.log("SUCCESS!");
+  };
+
+  return (
+    <Fragment>
+      <section className="container">
+        <h1 className="large text-primary">Sign In</h1>
+        <p className="lead">
+          <i className="fas fa-user"></i> Sign Into Your Account
+        </p>
+        <form className="form" onSubmit={(e) => onSubmit(e)}>
+          <div className="form-group">
+            <input
+              type="email"
+              placeholder="Email Address"
+              name="email"
+              value={email}
+              onChange={(e) => onChange(e)}
+              required
+            />
+            <small className="form-text">
+              This site uses Gravatar so if you want a profile image, use a
+              Gravatar email
+            </small>
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              placeholder="Password"
+              name="password"
+              value={password}
+              onChange={(e) => onChange(e)}
+              minLength="6"
+              required
+            />
+          </div>
+
+          <input type="submit" className="btn btn-primary" value="Register" />
+        </form>
+        <p className="my-1">
+          Don't have an account? <Link to="/register">Sign Up</Link>
+        </p>
+      </section>
+    </Fragment>
+  );
 };
 
 export default Login;
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,5 +1,5 @@
 import React, { Fragment, useState } from "react";
-import axios from "axios";
+import { Link } from "react-router-dom";
 
 const Register = () => {
   const [formData, setFromData] = useState({
@@ -20,28 +20,9 @@ const Register = () => {
     //Check if password inputs match // available to you via decunstructed fromData
     if (password !== password2) {
       console.log("Passwords do not match");
+      alert("Passwords do not match");
     } else {
-      const newUser = {
-        name,
-        email,
-        password,
-      };
-
-      try {
-        const config = {
-          headers: {
-            "Content-Type": "application/json",
-          },
-        };
-
-        const body = JSON.stringify(newUser);
-
-        const res = await axios.post("api/users", body, config);
-
-        console.log(res.data);
-      } catch (err) {
-        console.error(err.response.data);
-      }
+      console.log("SUCCESS!");
     }
   };
 
@@ -52,11 +33,7 @@ const Register = () => {
         <p className="lead">
           <i className="fas fa-user"></i> Create Your Account
         </p>
-        <form
-          className="form"
-          action="create-profile.html"
-          onSubmit={(e) => onSubmit(e)}
-        >
+        <form className="form" onSubmit={(e) => onSubmit(e)}>
           <div className="form-group">
             <input
               type="text"
@@ -106,7 +83,7 @@ const Register = () => {
           <input type="submit" className="btn btn-primary" value="Register" />
         </form>
         <p className="my-1">
-          Already have an account? <a href="login.html">Sign In</a>
+          Already have an account? <Link to="/login">Sign In</Link>
         </p>
       </section>
     </Fragment>
