03780d5e6e49668bc389c162735b80ba27ea8a79
Christian Sheen <csheen1725@gmail.com>
2021-05-06 13:13:36 -0400

    Users logging in with no profile will be greated with a screen that affords thema  button which <Link/> them to a page where they can go ahead and set up a profile (at this stage profile creation features are non-existent

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,4 +1,5 @@
 import React, { Fragment, useEffect } from "react";
+import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import Spinner from "../layout/Spinner";
@@ -24,7 +25,12 @@ const Dashboard = ({
       {profile !== null ? (
         <Fragment>has</Fragment>
       ) : (
-        <Fragment>has not</Fragment>
+        <Fragment>
+          <p>You have not yet setup a profile, please add some info</p>
+          <Link to="/create-profile" className="btn btn-primary my-1">
+            Create Profile
+          </Link>
+        </Fragment>
       )}
     </Fragment>
   );
