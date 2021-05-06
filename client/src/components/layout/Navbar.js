44ef7e0d8e5aefb678b870e79fa5d1ce60745ed3
Christian Sheen <csheen1725@gmail.com>
2021-05-06 10:50:54 -0400

    Created NavBar "Dashboar" Link tag to always redirect to dashboard which is a Redirect Tag within the landing component once the user "isAuthenticated" status is mactched as true

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -8,6 +8,12 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
   const authLinks = (
     <ul>
       <li>
+        <Link to="/dashboard">
+          <i className="fas fa-user" />{" "}
+          <span className="hide-sm"> Dashboard</span>
+        </Link>
+      </li>
+      <li>
         <Link onClick={logout} to="#!">
           <i className="fas fa-sign-out-alt" />{" "}
           <span className="hide-sm">Logout</span>
