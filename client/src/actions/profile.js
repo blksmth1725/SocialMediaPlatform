bbd73790c0e966541ca28a1086195149b3999a09
Christian Sheen <csheen1725@gmail.com>
2021-05-10 14:03:37 -0400

    Ability to display "experiences" on dashboard as well as delete them

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -122,3 +122,22 @@ export const addEducation = (formData, history) => async (dispatch) => {
     });
   }
 };
+
+// Delete experience
+export const deleteExperience = (id) => async (dispatch) => {
+  try {
+    const res = await axios.delete(`api/profile/experience/${id}`);
+
+    dispatch({
+      type: UPDATE_PROFILE,
+      payload: res.data,
+    });
+
+    dispatch(setAlert("Experience Removed", "success"));
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -4,6 +4,7 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import Spinner from "../layout/Spinner";
 import DashboardActions from "./DashboardActions";
+import Experience from "./Experience";
 import { getCurrentProfile } from "../../actions/profile";
 
 const Dashboard = ({
@@ -26,6 +27,7 @@ const Dashboard = ({
       {profile !== null ? (
         <Fragment>
           <DashboardActions />
+          <Experience experience={profile.experience} />
         </Fragment>
       ) : (
         <Fragment>
