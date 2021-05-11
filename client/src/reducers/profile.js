829e1fffbeae06086587a6390221a8fe49ddcd16
Christian Sheen <csheen1725@gmail.com>
2021-05-10 22:29:11 -0400

    Finished Profile actions && reducers

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -1,6 +1,8 @@
 import {
   CLEAR_PROFILE,
   GET_PROFILE,
+  GET_PROFILES,
+  GET_REPOS,
   PROFILE_ERROR,
   UPDATE_PROFILE,
 } from "../actions/types";
@@ -24,6 +26,12 @@ export default function (state = initialState, action) {
         profile: payload,
         loading: false,
       };
+    case GET_PROFILES:
+      return {
+        ...state,
+        profiles: payload,
+        loading: false,
+      };
     case PROFILE_ERROR:
       return {
         ...state,
@@ -35,6 +43,12 @@ export default function (state = initialState, action) {
         ...state,
         profile: null,
         repos: [],
+        loading: false,
+      };
+    case GET_REPOS:
+      return {
+        ...state,
+        repos: payload,
         loading: false,
       };
     default:
