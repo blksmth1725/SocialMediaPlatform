a5b493e91a4bf731068f203a74d980bae2161b2b
Christian Sheen <csheen1725@gmail.com>
2021-05-14 13:40:59 -0400

    Display profile top Card... MUST REVIEW PROCCESS

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -2,6 +2,7 @@ import React, { Fragment, useEffect } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import Spinner from "../layout/Spinner";
+import ProfileTop from "./ProfileTop";
 import { getProfileById } from "../../actions/profile";
 import { Link } from "react-router-dom";
 
@@ -13,7 +14,7 @@ const Profile = ({
 }) => {
   useEffect(() => {
     getProfileById(match.params.id);
-  }, [getProfileById]);
+  }, [getProfileById, match.params.id]);
 
   return (
     <Fragment>
@@ -31,6 +32,9 @@ const Profile = ({
                 Edit Profile
               </Link>
             )}
+          <div class="profile-grid my-1">
+            <ProfileTop profile={profile} />
+          </div>
         </Fragment>
       )}
     </Fragment>
