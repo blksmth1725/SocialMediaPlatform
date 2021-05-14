3f2fcc804d6c4f09421ec2ccd5b691138c3bfbef
Christian Sheen <csheen1725@gmail.com>
2021-05-14 13:55:21 -0400

    allowed for all icons in profileTop to go to social media pages

--- client/src/components/profile/ProfileTop.js
+++ client/src/components/profile/ProfileTop.js
@@ -18,23 +18,44 @@ const ProfileTop = ({
       <p className="lead">
         {status} {company && <span> at {company}</span>}
       </p>
-      <p>Seattle, WA</p>
+      <p>{location && <span>{location}</span>}</p>
       <div className="icons my-1">
-        <a href="#" target="_blank" rel="noopener noreferrer">
-          <i className="fas fa-globe fa-2x"></i>
-        </a>
-        <a href="#" target="_blank" rel="noopener noreferrer">
-          <i className="fab fa-twitter fa-2x"></i>
-        </a>
-        <a href="#" target="_blank" rel="noopener noreferrer">
-          <i className="fab fa-facebook fa-2x"></i>
-        </a>
-        <a href="#" target="_blank" rel="noopener noreferrer">
-          <i className="fab fa-linkedin fa-2x"></i>
-        </a>
-        <a href="#" target="_blank" rel="noopener noreferrer">
-          <i className="fab fa-youtube fa-2x"></i>
-        </a>
+        {website && (
+          <a href={website} target="_blank" rel="noopener noreferrer">
+            <i className="fas fa-globe fa-2x"></i>
+          </a>
+        )}
+
+        {social && social.twitter && (
+          <a href={social.twitter} target="_blank" rel="noopener noreferrer">
+            <i className="fab fa-twitter fa-2x"></i>
+          </a>
+        )}
+        {social && social.twitter && (
+          <a href={social.twitter} target="_blank" rel="noopener noreferrer">
+            <i className="fab fa-twitter fa-2x"></i>
+          </a>
+        )}
+        {social && social.facebook && (
+          <a href={social.facebook} target="_blank" rel="noopener noreferrer">
+            <i className="fab fa-facebook fa-2x"></i>
+          </a>
+        )}
+        {social && social.linkedin && (
+          <a href={social.linkedin} target="_blank" rel="noopener noreferrer">
+            <i className="fab fa-linkedin fa-2x"></i>
+          </a>
+        )}
+        {social && social.youtube && (
+          <a href={social.youtube} target="_blank" rel="noopener noreferrer">
+            <i className="fab fa-youtube fa-2x"></i>
+          </a>
+        )}
+        {social && social.instagram && (
+          <a href={social.instagram} target="_blank" rel="noopener noreferrer">
+            <i className="fab fa-instagram fa-2x"></i>
+          </a>
+        )}
         <a href="#" target="_blank" rel="noopener noreferrer">
           <i className="fab fa-instagram fa-2x"></i>
         </a>
