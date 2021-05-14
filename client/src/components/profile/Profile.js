04b5d5cb3779667c599ff51337129f738c7d86eb
Christian Sheen <csheen1725@gmail.com>
2021-05-14 14:15:24 -0400

    Dynamically rendering skills on ProfileAbout Card feature

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import Spinner from "../layout/Spinner";
 import ProfileTop from "./ProfileTop";
+import ProfileAbout from "./ProfileAbout";
 import { getProfileById } from "../../actions/profile";
 import { Link } from "react-router-dom";
 
@@ -34,6 +35,7 @@ const Profile = ({
             )}
           <div class="profile-grid my-1">
             <ProfileTop profile={profile} />
+            <ProfileAbout profile={profile} />
           </div>
         </Fragment>
       )}
