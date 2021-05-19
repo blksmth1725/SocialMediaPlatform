9d84500ced82303b27614d33a24f242969e6ac5d
Christian Sheen <csheen1725@gmail.com>
2021-05-19 00:09:21 -0400

    resolved display of single post in ProfileItem after conditional logic

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -12,63 +12,57 @@ const PostItem = ({
   auth,
   post: { _id, text, name, avatar, user, likes, comments, date },
   showActions,
-}) => {
-  return (
-    <div className="post bg-white p-1 my-1">
-      <div>
-        <Link to={`/profile/${user}`}>
-          <img className="round-img" src={avatar} alt="" />
-          <h4>{name}</h4>
-        </Link>
-      </div>
-      <div>
-        <p className="my-1">{text}</p>
-        <p className="post-date">Posted on {formatDate(date)}</p>
-        {showActions && (
-          <Fragment>
-            <button
-              onClick={(e) => addLike(_id)}
-              type="button"
-              className="btn btn-light"
-            >
-              <i className="fas fa-thumbs-up"></i>{" "}
-              <span>
-                {likes.length > 0 && (
-                  <span className="comment-count">{likes.length}</span>
-                )}
-              </span>
-            </button>
-            <button
-              onClick={(e) => removeLike(_id)}
-              type="button"
-              className="btn btn-light"
-            >
-              <i className="fas fa-thumbs-down"></i>
-            </button>
-            <Link to={`/posts/${_id}`} className="btn btn-primary">
-              Discussion{" "}
-              {comments.length > 0 && (
-                <span className="comment-count">{comments.length}</span>
-              )}
-            </Link>
-
-            {!auth.loading && user === auth.user._id && (
-              <button
-                onClick={(e) => deletePost(_id)}
-                type="button"
-                className="btn btn-danger"
-              >
-                <i className="fas fa-times"></i>
-              </button>
-            )}
-          </Fragment>
-        )}
-      </div>
+}) => (
+  <div className="post bg-white p-1 my-1">
+    <div>
+      <Link to={`/profile/${user}`}>
+        <img className="round-img" src={avatar} alt="" />
+        <h4>{name}</h4>
+      </Link>
     </div>
-  );
-};
+    <div>
+      <p className="my-1">{text}</p>
+      <p className="post-date">Posted on {formatDate(date)}</p>
 
-PostItem.defualtProps = {
+      {showActions && (
+        <Fragment>
+          <button
+            onClick={() => addLike(_id)}
+            type="button"
+            className="btn btn-light"
+          >
+            <i className="fas fa-thumbs-up" />{" "}
+            <span>{likes.length > 0 && <span>{likes.length}</span>}</span>
+          </button>
+          <button
+            onClick={() => removeLike(_id)}
+            type="button"
+            className="btn btn-light"
+          >
+            <i className="fas fa-thumbs-down" />
+          </button>
+          <Link to={`/posts/${_id}`} className="btn btn-primary">
+            Discussion{" "}
+            {comments.length > 0 && (
+              <span className="comment-count">{comments.length}</span>
+            )}
+          </Link>
+          {!auth.loading && user === auth.user._id && (
+            <button
+              onClick={() => deletePost(_id)}
+              type="button"
+              className="btn btn-danger"
+            >
+              <i className="fas fa-times" />
+            </button>
+          )}
+        </Fragment>
+      )}
+    </div>
+  </div>
+);
+
+PostItem.defaultProps = {
   showActions: true,
 };
 
