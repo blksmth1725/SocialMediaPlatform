8444b3fc38e12e803a4e29ef6ba9b50073ee8893
Christian Sheen <csheen1725@gmail.com>
2021-05-18 22:26:52 -0400

    actions and reducers created to add Posts... included a form which wired up everything and maintained component level state pertaining to the text input by the user. also added functionality which allowed users to navigate to post publishers profile if onClick on avatar

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -1,6 +1,12 @@
 import axios from "axios";
 import { setAlert } from "./alert";
-import { GET_POSTS, POST_ERROR, UPDATE_LIKES, DELETE_POST } from "./types";
+import {
+  GET_POSTS,
+  POST_ERROR,
+  UPDATE_LIKES,
+  DELETE_POST,
+  ADD_POST,
+} from "./types";
 
 //Get Posts
 export const getPosts = () => async (dispatch) => {
@@ -58,7 +64,7 @@ export const removeLike = (id) => async (dispatch) => {
   }
 };
 
-//Remove like
+//Delete Post
 export const deletePost = (id) => async (dispatch) => {
   try {
     await axios.delete(`/api/posts/${id}`);
@@ -67,6 +73,29 @@ export const deletePost = (id) => async (dispatch) => {
       payload: id,
     });
     dispatch(setAlert("Post Removed", "success"));
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
+
+//Add Post
+export const addPost = (formData) => async (dispatch) => {
+  const config = {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+
+  try {
+    const res = await axios.post("/api/posts", formData, config);
+    dispatch({
+      type: ADD_POST,
+      payload: res.data,
+    });
+    dispatch(setAlert("Post Created", "success"));
   } catch (err) {
     dispatch({
       type: POST_ERROR,
--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import Spinner from "../layout/Spinner";
 import PostItem from "./PostItem";
+import PostForm from "./PostForm";
 import { getPosts } from "../../actions/post";
 
 const Posts = ({ getPosts, post: { posts, loading } }) => {
@@ -18,7 +19,7 @@ const Posts = ({ getPosts, post: { posts, loading } }) => {
       <p className="lead">
         <i className="fas fa-user"></i> Welcome to the community
       </p>
-      {/* Post Form */}
+      <PostForm />
       <div className="posts">
         {posts.map((post) => (
           <PostItem key={post._id} post={post} />
